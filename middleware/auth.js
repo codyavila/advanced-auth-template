5802022c70dbf1dd1faa9da18b5c518707500430
sanderscody26 <sanderscody26@gmail.com>
2022-08-23 15:32:33 -0400

    Functioning Authorized Routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/User.js')
 const ErrorResponse = require('../utils/errorResponse')
 
-exports.protect = async (req, res, next) {
+exports.protect = async (req, res, next) => {
     let token
 
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
