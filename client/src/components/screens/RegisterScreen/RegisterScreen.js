3f08fc0c0f9876048496d9b352db82d98480bf3d
sanderscody26 <sanderscody26@gmail.com>
2022-08-23 17:08:41 -0400

    Cleaned React-app, added components and styles

---
+++ client/src/components/screens/RegisterScreen/RegisterScreen.js
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
+import './registerScreen.css'
+
+const RegisterScreen = () => {
+  return (
+    
+  )
+}
+
+export default RegisterScreen
