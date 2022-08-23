b2df11515f1db3a9627d442e4023a7d7d5b2d2e4
sanderscody26 <sanderscody26@gmail.com>
2022-08-23 14:28:11 -0400

    Implemented error handler middlware

--- middleware/error.js
+++ middleware/error.js
@@ -4,8 +4,6 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err }
   error.message = err.message
 
-  console.log(err)
-
   if (err.code === 11000) {
     const message = `Duplicate Field Value Enter`
     error = new ErrorResponse(message, 400)
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: './config.env' })
 const express = require('express')
 const connectDB = require('./config/db.js')
+const errorHandler = require('./middleware/error.js')
 
 // Connect DB
 connectDB()
@@ -9,8 +10,10 @@ const app = express()
 
 app.use(express.json())
 
-
 app.use('/api/auth', require('./routes/auth.js'))
+
+// Error Handler (Should be last piece of middleware)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
